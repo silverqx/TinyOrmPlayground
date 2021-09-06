01ae9dabd8b247976a0e8f59bacee1e3fe53e9a2
silverqx <silver.zachara@gmail.com>
2021-09-06 08:27:20 +0200

    finished cmake build

---
+++ cmake/Modules/TinyInitDefaultVariables.cmake
@@ -0,0 +1,30 @@
+include(TinyHelpers)
+
+# Initialize CMake default variables by project options
+function(tiny_init_cmake_variables)
+
+    set(CMAKE_FIND_PACKAGE_SORT_ORDER NATURAL)
+    set(CMAKE_FIND_PACKAGE_SORT_DIRECTION DEC)
+    set(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION YES)
+
+    tiny_invert_bool(PRECOMPILE_HEADERS ${PRECOMPILE_HEADERS})
+    set(CMAKE_DISABLE_PRECOMPILE_HEADERS ${PRECOMPILE_HEADERS})
+
+    if(MSVC_RUNTIME_DYNAMIC)
+        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+    else()
+        set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+    endif()
+
+    # Fix install prefix for the x64 toolchain
+    if(WIN32 AND CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT
+            AND CMAKE_SIZEOF_VOID_P EQUAL 8
+    )
+        get_property(intall_prefix_docs CACHE CMAKE_INSTALL_PREFIX PROPERTY HELPSTRING)
+
+        set(CMAKE_INSTALL_PREFIX "C:/Program Files/${PROJECT_NAME}"
+            CACHE PATH "${intall_prefix_docs}" FORCE
+        )
+    endif()
+
+endfunction()
---
+++ cmake/Modules/TinySources.cmake
@@ -0,0 +1,21 @@
+# TinyOrmPlayground header and source files
+# Create header and source files lists and return them
+function(tiny_sources)
+
+    set(headers
+        common.hpp
+        testorm.hpp
+    )
+
+    set(sources
+        main.cpp
+        testorm.cpp
+    )
+
+    list(TRANSFORM headers PREPEND "src/")
+    list(TRANSFORM sources PREPEND "src/")
+
+    set(headers ${headers} PARENT_SCOPE)
+    set(sources ${sources} PARENT_SCOPE)
+
+endfunction()
