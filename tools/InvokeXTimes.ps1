b61868352f39eca48948fefd4fb48a409ba062b7
silverqx <silver.zachara@gmail.com>
2021-11-16 19:20:10 +0100

    exchanged arguments for InvokeXTimes

--- tools/InvokeXTimes.ps1
+++ tools/InvokeXTimes.ps1
@@ -1,15 +1,14 @@
 Set-StrictMode -Version 3.0
 
-$Script:ExeToInvoke = '..\..\TinyOrmPlayground-builds-qmake\build-TinyOrmPlayground-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug\debug\TinyOrmPlayground.exe'
-if ($args.Count -eq 1)
-{
-    $Script:ExeToInvoke = $args[0]
+$Script:XTimes = 2
+if ($args.Count -eq 1) {
+    $Script:XTimes = $args[0]
 }
 
-$Script:XTimes = 2
+$Script:ExeToInvoke = '..\..\TinyOrmPlayground-builds-qmake\build-TinyOrmPlayground-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug\debug\TinyOrmPlayground.exe'
 if ($args.Count -eq 2)
 {
-    $Script:XTimes = $args[1]
+    $Script:ExeToInvoke = $args[1]
 }
 
 for ($i = 0; $i -lt $Script:XTimes; $i++)
