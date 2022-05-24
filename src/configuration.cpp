243ae7c37f1b11cef892d2d1f6a7684d59039e33
silverqx <silver.zachara@gmail.com>
2022-05-24 13:40:30 +0200

    used empty as string constant

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -8,10 +8,14 @@
 
 #include "config.hpp"
 
+using Orm::Constants::EMPTY;
 using Orm::Constants::H127001;
+using Orm::Constants::InnoDB;
+using Orm::Constants::LOCAL;
 using Orm::Constants::LOCALHOST;
 using Orm::Constants::P3306;
 using Orm::Constants::P5432;
+using Orm::Constants::PUBLIC;
 using Orm::Constants::QMYSQL;
 using Orm::Constants::QPSQL;
 using Orm::Constants::QSQLITE;
@@ -26,15 +30,12 @@ using Orm::Constants::driver_;
 using Orm::Constants::engine_;
 using Orm::Constants::foreign_key_constraints;
 using Orm::Constants::host_;
-using Orm::Constants::InnoDB;
 using Orm::Constants::isolation_level;
-using Orm::Constants::LOCAL;
 using Orm::Constants::options_;
 using Orm::Constants::password_;
 using Orm::Constants::port_;
 using Orm::Constants::prefix_;
 using Orm::Constants::prefix_indexes;
-using Orm::Constants::PUBLIC;
 using Orm::Constants::schema_;
 using Orm::Constants::strict_;
 using Orm::Constants::timezone_;
@@ -78,8 +79,8 @@ const Configuration::OrmConfigurationsType &Configuration::initDBConfigurations(
         // Main SQLite connection in test loop
         {Sqlite, {
             {driver_,    QSQLITE},
-            {database_,  qEnvironmentVariable("DB_SQLITE_DATABASE", "")},
-            {prefix_,    ""},
+            {database_,  qEnvironmentVariable("DB_SQLITE_DATABASE", EMPTY)},
+            {prefix_,    EMPTY},
             {options_,   QVariantHash()},
             {foreign_key_constraints, qEnvironmentVariable("DB_SQLITE_FOREIGN_KEYS",
                                                            QStringLiteral("true"))},
@@ -90,7 +91,7 @@ const Configuration::OrmConfigurationsType &Configuration::initDBConfigurations(
         {Sqlite_Memory, {
             {driver_,    QSQLITE},
             {database_,  QStringLiteral(":memory:")},
-            {prefix_,    ""},
+            {prefix_,    EMPTY},
             {options_,   QVariantHash()},
             {foreign_key_constraints, qEnvironmentVariable("DB_SQLITE_FOREIGN_KEYS",
                                                            QStringLiteral("true"))},
@@ -117,14 +118,14 @@ const Configuration::OrmConfigurationsType &Configuration::initDBConfigurations(
             {driver_,   QPSQL},
             {host_,     qEnvironmentVariable("DB_PGSQL_HOST",     H127001)},
             {port_,     qEnvironmentVariable("DB_PGSQL_PORT",     P5432)},
-            {database_, qEnvironmentVariable("DB_PGSQL_DATABASE", "")},
+            {database_, qEnvironmentVariable("DB_PGSQL_DATABASE", EMPTY)},
             {schema_,   qEnvironmentVariable("DB_PGSQL_SCHEMA",   PUBLIC)},
             {username_, qEnvironmentVariable("DB_PGSQL_USERNAME", "postgres")},
-            {password_, qEnvironmentVariable("DB_PGSQL_PASSWORD", "")},
+            {password_, qEnvironmentVariable("DB_PGSQL_PASSWORD", EMPTY)},
             {charset_,  qEnvironmentVariable("DB_PGSQL_CHARSET",  UTF8)},
             // I don't use timezone types in postgres anyway
             {timezone_,       LOCAL},
-            {prefix_,         ""},
+            {prefix_,         EMPTY},
             {prefix_indexes,  true},
             {options_,        QVariantHash(/*{{"requiressl", 1}}*/)},
         }},
@@ -139,15 +140,15 @@ QVariantHash Configuration::initMysqlConfiguration()
         {driver_,    QMYSQL},
         {host_,      qEnvironmentVariable("DB_MYSQL_HOST", H127001)},
         {port_,      qEnvironmentVariable("DB_MYSQL_PORT", P3306)},
-        {database_,  qEnvironmentVariable("DB_MYSQL_DATABASE", "")},
-        {username_,  qEnvironmentVariable("DB_MYSQL_USERNAME", "")},
-        {password_,  qEnvironmentVariable("DB_MYSQL_PASSWORD", "")},
+        {database_,  qEnvironmentVariable("DB_MYSQL_DATABASE", EMPTY)},
+        {username_,  qEnvironmentVariable("DB_MYSQL_USERNAME", EMPTY)},
+        {password_,  qEnvironmentVariable("DB_MYSQL_PASSWORD", EMPTY)},
         {charset_,   qEnvironmentVariable("DB_MYSQL_CHARSET", UTF8MB4)},
         {collation_, qEnvironmentVariable("DB_MYSQL_COLLATION",
                                           QStringLiteral("utf8mb4_0900_ai_ci"))},
         // CUR add timezone names to the MySQL server and test them silverqx
         {timezone_,       SYSTEM},
-        {prefix_,         ""},
+        {prefix_,         EMPTY},
         {prefix_indexes,  true},
         {strict_,         true},
         {isolation_level, QStringLiteral("REPEATABLE READ")},
@@ -167,14 +168,14 @@ QVariantHash Configuration::initMysqlLaravel8Configuration()
         {driver_,    QMYSQL},
         {host_,      qEnvironmentVariable("DB_MYSQL_LARAVEL_HOST", H127001)},
         {port_,      qEnvironmentVariable("DB_MYSQL_LARAVEL_PORT", P3306)},
-        {database_,  qEnvironmentVariable("DB_MYSQL_LARAVEL_DATABASE", "")},
-        {username_,  qEnvironmentVariable("DB_MYSQL_LARAVEL_USERNAME", "")},
-        {password_,  qEnvironmentVariable("DB_MYSQL_LARAVEL_PASSWORD", "")},
+        {database_,  qEnvironmentVariable("DB_MYSQL_LARAVEL_DATABASE", EMPTY)},
+        {username_,  qEnvironmentVariable("DB_MYSQL_LARAVEL_USERNAME", EMPTY)},
+        {password_,  qEnvironmentVariable("DB_MYSQL_LARAVEL_PASSWORD", EMPTY)},
         {charset_,   qEnvironmentVariable("DB_MYSQL_LARAVEL_CHARSET", UTF8MB4)},
         {collation_, qEnvironmentVariable("DB_MYSQL_LARAVEL_COLLATION",
                                           QStringLiteral("utf8mb4_0900_ai_ci"))},
         {timezone_,       SYSTEM},
-        {prefix_,         ""},
+        {prefix_,         EMPTY},
         {prefix_indexes,  true},
         {strict_,         true},
         {isolation_level, QStringLiteral("REPEATABLE READ")},
@@ -207,7 +208,7 @@ void Configuration::minimizeMysqlTimeouts(QVariantHash &connectionOptions)
 
 QString Configuration::initCheckDatabaseExistsFile()
 {
-    auto path = qEnvironmentVariable("DB_SQLITE_DATABASE", "");
+    auto path = qEnvironmentVariable("DB_SQLITE_DATABASE", EMPTY);
 
     if (path.isEmpty())
         throw std::runtime_error(
